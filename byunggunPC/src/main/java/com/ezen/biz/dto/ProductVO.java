2dec99988defb604928d83486a7d181a1b990ce8
jeoncell <66252147+jeoncell@users.noreply.github.com>
2023-03-28 11:17:17 +0900

    lombok 이슈

---
+++ byunggunPC/src/main/java/com/ezen/biz/dto/ProductVO.java
@@ -0,0 +1,24 @@
+package com.ezen.biz.dto;
+
+import java.util.Date;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ProductVO {
+	private int pseq;
+	private String name;
+	private String kind;
+	private int price1;
+	private int price2;
+	private int price3;
+	private String content;
+	private String image;
+	private String bestyn;
+	private int quantity;
+	private Date regdate;
+}
